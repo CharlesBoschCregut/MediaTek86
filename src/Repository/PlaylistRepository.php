e60282d1c623643fa185830103b387fcb23b78d8
Ufail77 <47165135+Ufail77@users.noreply.github.com>
2023-03-11 05:49:32 +0100

    Mission 1: Tâche 2 Respect des bonnes pratiques de codage Task #3 - Mission 1 : Tâche 2 : respecter les bonnes pratiques de codage

--- src/Repository/PlaylistRepository.php
+++ src/Repository/PlaylistRepository.php
@@ -71,48 +71,56 @@ class PlaylistRepository extends ServiceEntityRepository
      * ou tous les enregistrements si la valeur est vide
      * @param type $champ
      * @param type $valeur
-     * @param type $table si $champ dans une autre table
      * @return Playlist[]
      */
-    public function findByContainValue($champ, $valeur, $table=""): array
+    public function findByContainValue($champ, $valeur): array
     {
         if ($valeur=="") {
             return $this->findAllOrderBy('name', 'ASC');
         }
-        if ($table=="") {
-            return $this->createQueryBuilder('p')
-                    ->select(self::SELECT_PARAM)
-                    ->addSelect(self::ADDSELECT_NAME)
-                    ->addSelect(self::ADDSELECT_CATEGORIE)
-                    ->leftjoin('p.formations', 'f')
-                    ->leftjoin('f.categories', 'c')
-                    ->where('p.'.$champ.' LIKE :valeur')
-                    ->setParameter('valeur', '%'.$valeur.'%')
-                    ->groupBy('p.id')
-                    ->addGroupBy('c.name')
-                    ->orderBy('p.name', 'ASC')
-                    ->addOrderBy('c.name')
-                    ->getQuery()
-                    ->getResult();
-        } else {
-            return $this->createQueryBuilder('p')
-                    ->select(self::SELECT_PARAM)
-                    ->addSelect(self::ADDSELECT_NAME)
-                    ->addSelect(self::ADDSELECT_CATEGORIE)
-                    ->leftjoin('p.formations', 'f')
-                    ->leftjoin('f.categories', 'c')
-                    ->where('c.'.$champ.' LIKE :valeur')
-                    ->setParameter('valeur', '%'.$valeur.'%')
-                    ->groupBy('p.id')
-                    ->addGroupBy('c.name')
-                    ->orderBy('p.name', 'ASC')
-                    ->addOrderBy('c.name')
-                    ->getQuery()
-                    ->getResult();
-            
-        }
+        return $this->createQueryBuilder('p')
+            ->select(self::SELECT_PARAM)
+            ->addSelect(self::ADDSELECT_NAME)
+            ->addSelect(self::ADDSELECT_CATEGORIE)
+            ->leftjoin('p.formations', 'f')
+            ->leftjoin('f.categories', 'c')
+            ->where('p.'.$champ.' LIKE :valeur')
+            ->setParameter('valeur', '%'.$valeur.'%')
+            ->groupBy('p.id')
+            ->addGroupBy('c.name')
+            ->orderBy('p.name', 'ASC')
+            ->addOrderBy('c.name')
+            ->getQuery()
+            ->getResult();
     }
-
-
     
+    
+    /**
+    * Enregistrements dont un champ contient une valeur
+    * ou tous les enregistrements si la valeur est vide
+    * @param type $champ
+    * @param type $valeur
+    * @param type $table
+    * @return Playlist[]
+    */
+    public function findByContainValueInTable($champ, $valeur, $table=""): array
+    {
+        if ($valeur=="") {
+            return $this->findAllOrderBy('name', 'ASC');
+        }
+        return $this->createQueryBuilder('p')
+            ->select(self::SELECT_PARAM)
+            ->addSelect(self::ADDSELECT_NAME)
+            ->addSelect(self::ADDSELECT_CATEGORIE)
+            ->leftjoin('p.formations', 'f')
+            ->leftjoin('f.categories', 'c')
+            ->where('c.'.$champ.' LIKE :valeur')
+            ->setParameter('valeur', '%'.$valeur.'%')
+            ->groupBy('p.id')
+            ->addGroupBy('c.name')
+            ->orderBy('p.name', 'ASC')
+            ->addOrderBy('c.name')
+            ->getQuery()
+            ->getResult();
+    }
 }
