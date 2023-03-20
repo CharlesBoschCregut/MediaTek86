20bbf8e340171e420442d7fca28a9495113e5e54
Ufail77 <47165135+Ufail77@users.noreply.github.com>
2023-03-20 12:36:31 +0100

    Mission 2: Tâche 1 Gérer les formations Ajout controle des saisies Task #5 - Mission 2 : Tâche 1 : gérer les formations

--- src/Entity/Formation.php
+++ src/Entity/Formation.php
@@ -7,6 +7,7 @@ use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=FormationRepository::class)
@@ -27,11 +28,13 @@ class Formation
 
     /**
      * @ORM\Column(type="datetime", nullable=true)
+     * @Assert\DateTime
      */
     private $publishedAt;
 
     /**
      * @ORM\Column(type="string", length=100, nullable=true)
+     * @Assert\Length(min = 0, max = 100)
      */
     private $title;
 
@@ -42,6 +45,7 @@ class Formation
 
     /**
      * @ORM\Column(type="string", length=20, nullable=true)
+     * @Assert\Length(min = 0, max = 20)
      */
     private $videoId;
 
--- src/Form/FormationType.php
+++ src/Form/FormationType.php
@@ -7,6 +7,8 @@ use App\Entity\Playlist;
 use DateTime;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,10 +20,10 @@ class FormationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', null, [
+            ->add('title', TextType::class, [
                 'label' => 'Titre :'
             ])
-            ->add('description', null, [
+            ->add('description', TextareaType::class, [
                 'label' => 'Description :',
                 'required' => false
             ])
@@ -42,7 +44,7 @@ class FormationType extends AbstractType
                  'days' => range(1, 31),
             ])
                 
-            ->add('videoId', null, [
+            ->add('videoId', TextType::class, [
                 'label' => 'video id'
             ])
             ->add('submit', SubmitType::class, [
@@ -50,11 +52,6 @@ class FormationType extends AbstractType
             ])
         ;
     }
-    
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Formation::class,
-        ]);
-    }
 }
+
+
